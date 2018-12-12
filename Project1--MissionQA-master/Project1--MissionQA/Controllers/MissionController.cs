acf04a3d9dead7cc25d52f0cc71610f4d9331358
Jinlin <zhujl97@gmail.com>
2018-12-11 18:49:14 -0700

    upload file

---
+++ Project1--MissionQA-master/Project1--MissionQA/Controllers/MissionController.cs
@@ -0,0 +1,63 @@
+﻿using Project1__MissionQA.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project1__MissionQA.Controllers
+{
+    public class MissionController : Controller
+    {
+        public List<string> listNames = new List<string> { "Brazil Florianópolis", "Puerto Rico San Juan", "New York New York" };
+
+        public int MissionNumber { get; set; }
+
+        // GET: Mission
+        public ActionResult Missions()
+        {
+            ViewBag.missions = listNames;
+            return View();
+        }
+
+        //POST: Mission
+        [HttpPost]
+        public ActionResult YourMission(Mission mission)
+        {
+            if (mission.Name == "Brazil Florianópolis")
+            {
+                mission.President = "Ramilfo Silva";
+                mission.Address = "Santa Catarina";
+                mission.Language = "Portuguese";
+                mission.Climate = "Local steppe";
+                mission.DominantReligion = "Catholicism";
+                mission.Symbol = "file-location-of-the-flag-image";
+
+                return View(mission);
+            }
+            else if (mission.Name == "Puerto Rico San Juan")
+            {
+                mission.President = "President Boucher";
+                mission.Address = "Puerto Rico";
+                mission.Language = "Spanish";
+                mission.Climate = "Tropical";
+                mission.DominantReligion = "Catholicism";
+                mission.Symbol = "file-location-of-the-flag-image";
+
+                return View(mission);
+            }
+            else
+            {
+                mission.President = "Nelson";
+                mission.Address = "New York";
+                mission.Language = "Multiple";
+                mission.Climate = "Humid subtropical";
+                mission.DominantReligion = "Protestantism";
+                mission.Symbol = "file-location-of-the-big-apple";
+
+                return View(mission);
+            }
+        }
+    }
+}
---
+++ Project1--MissionQA-master/Project1--MissionQA/Models/Mission.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Project1__MissionQA.Models
+{
+    public class Mission
+    {
+        [Display(Name = "Mission Name")]
+        public string Name { get; set; }
+
+        public string President { get; set; }
+
+        public string Address { get; set; }
+
+        public string Language { get; set; }
+
+        public string Climate { get; set; }
+
+        public string DominantReligion { get; set; }
+
+        public string Symbol { get; set; }
+
+    }
+}
